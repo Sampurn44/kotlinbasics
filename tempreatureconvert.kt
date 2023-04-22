3bf69f67f64e92f4089fc264db48e2d13c075732
Sampurn Chouksey <sampurn10chouksey@gmail.com>
2023-04-22 13:10:14 +0530

    initial commit

---
+++ tempreatureconvert.kt
@@ -0,0 +1,16 @@
+fun main() {
+    printFinalTemperature(27.0,"Celsius","Fahrenheit"){9/5*(it)+32}
+    printFinalTemperature(350.0, "Kelvin", "Celsius") { it - 273.15 }
+    printFinalTemperature(10.0, "Fahrenheit", "Kelvin") { 5.0 / 9.0 * (it - 32) + 273.15 }
+}
+
+
+fun printFinalTemperature(
+    initialMeasurement: Double, 
+    initialUnit: String, 
+    finalUnit: String, 
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
+}
